4281818556a259baf76941bc21e0a387b5dcfa86
Marek Tresnak <marek.tresnak@outlook.com>
2020-03-16 17:41:55 +0100

    Update project file format

--- src/Mews.Fiscalization.Spain/Properties/AssemblyInfo.cs
+++ src/Mews.Fiscalization.Spain/Properties/AssemblyInfo.cs
@@ -1,37 +1,4 @@
-﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Mews.Fiscalization.Spain")]
-[assembly: AssemblyDescription("A package for issued invoices for spanish fiscalization. (Sii)")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Mews")]
-[assembly: AssemblyProduct("Mews.Fiscalization.Spain")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("db8b78bc-6e26-4aed-8a0b-ac1d22f2eddd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.9.0")]
-[assembly: AssemblyFileVersion("0.9.9.0")]
 [assembly: InternalsVisibleTo("Mews.Fiscalization.Spain.Tests")]
+
