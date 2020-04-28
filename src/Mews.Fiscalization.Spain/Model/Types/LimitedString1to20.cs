7dfba749524a18600d808b5e634dbacfbc4228b0
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-04-28 13:45:36 +0200

    Introduced XML validation.

--- src/Mews.Fiscalization.Spain/Model/Types/LimitedString1to20.cs
+++ src/Mews.Fiscalization.Spain/Model/Types/LimitedString1to20.cs
@@ -1,4 +1,6 @@
-﻿namespace Mews.Fiscalization.Spain.Model
+﻿using System.Xml;
+
+namespace Mews.Fiscalization.Spain.Model
 {
     public class LimitedString1to20 : LimitedString
     {
@@ -10,6 +12,19 @@
         public static bool IsValid(string value)
         {
             return IsValid(value, minLength: 1, maxLength: 20);
+        }
+
+        public static bool ContainsInvalidCharacters(string value)
+        {
+            try
+            {
+                XmlConvert.VerifyXmlChars(value);
+                return true;
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
         }
     }
 }
