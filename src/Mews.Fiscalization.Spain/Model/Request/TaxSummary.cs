66119680da952cc860e45c11110b47277f7b5826
Abdallah Altrabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2021-03-09 14:11:27 +0100

    Fixed possible NRE in tax summary. (#23)
    Co-authored-by: abdallahbeshi <abdallah.altrabeishi@mews.com>

--- src/Mews.Fiscalization.Spain/Model/Request/TaxSummary.cs
+++ src/Mews.Fiscalization.Spain/Model/Request/TaxSummary.cs
@@ -25,11 +25,11 @@ namespace Mews.Fiscalization.Spain.Model.Request
                 return Try.Error<TaxSummary, IEnumerable<Error>>(Error.Create("Tax summary must contain at least one item."));
             }
 
-            var validTaxExemptItems = taxExempt.AsNonEmpty().Match(
+            var validTaxExemptItems = taxExempt.ToOption().FlatMap(e => e.AsNonEmpty()).Match(
                 items => items.ToTry(i => i.Count() <= 7, _ => Error.Create("There can only be up to 7 tax exempt items on one invoice.")),
                 _ => Try.Success<INonEmptyEnumerable<TaxExemptItem>, INonEmptyEnumerable<Error>>(null)
             );
-            var validTaxRateSummaries = taxed.AsNonEmpty().Match(
+            var validTaxRateSummaries = taxed.ToOption().FlatMap(t => t.AsNonEmpty()).Match(
                 summaries => summaries.ToTry(s => s.Count() <= 6, _ => Error.Create("There can only be up to 6 distinct taxed items on one invoice.")),
                 _ => Try.Success<INonEmptyEnumerable<TaxRateSummary>, INonEmptyEnumerable<Error>>(null)
             );
